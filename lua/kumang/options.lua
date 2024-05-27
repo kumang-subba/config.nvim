529234e4542f3f33dd586c11137accdf46795832
Kumang Subba <kumangsubba@gmail.com>
2024-05-27 16:49:32 +0300

    set list true

--- lua/kumang/options.lua
+++ lua/kumang/options.lua
@@ -9,11 +9,10 @@ opt.ignorecase = true
 opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
-
 opt.smartindent = true
 
 opt.wrap = false
@@ -39,7 +38,9 @@ opt.clipboard = "unnamedplus"
 opt.updatetime = 50
 opt.colorcolumn = "0"
 
-opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
 vim.o.laststatus = 3
 vim.api.nvim_set_hl(0, "highlightbg", { ctermbg = 0, bg = "#999999" })
 
@@ -49,4 +50,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank({ timeout = 100, higroup = "highlightbg" })
 	end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
+	desc = "Disable new line comment",
 })
