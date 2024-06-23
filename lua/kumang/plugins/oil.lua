41b096d318c53e95ec864d83d84a2a9a53ca1413
Kumang Subba <kumangsubba@gmail.com>
2024-06-23 17:55:24 +0300

    oil.nvim update

--- lua/kumang/plugins/oil.lua
+++ lua/kumang/plugins/oil.lua
@@ -11,6 +11,9 @@ return {
 				},
 				view_options = {
 					show_hidden = true,
+					is_always_hidden = function(name, _)
+						return name == ".." or name == ".git" or name == "node_modules"
+					end,
 				},
 			})
 
